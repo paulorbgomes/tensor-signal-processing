fa7bc0bfe93f58b3e7f42921f7c4c51c9648e2d4
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2025-02-24 16:43:16 -0300

    Conclusion of homework-9

--- 005-multilinear-algebra-homeworks/homework-9/functions.py
+++ 005-multilinear-algebra-homeworks/homework-9/functions.py
@@ -164,13 +164,13 @@ def ten3_tucker3(tenG,A,B,C):
 def ten3_hosvd(tenX,R1,R2,R3):
     K,I,J = tenX.shape
     # Mode-1 ...
-    U1,S1,Vh1 = np.linalg.svd(ten3_unfold(tenX,1), full_matrices=False)
+    U1,_,_ = np.linalg.svd(ten3_unfold(tenX,1), full_matrices=False)
     U1 = U1[:,0:R1]
     # Mode-2 ...
-    U2,S2,Vh2 = np.linalg.svd(ten3_unfold(tenX,2), full_matrices=False)
+    U2,_,_ = np.linalg.svd(ten3_unfold(tenX,2), full_matrices=False)
     U2 = U2[:,0:R2]
     # Mode-3 ...
-    U3,S3,Vh3 = np.linalg.svd(ten3_unfold(tenX,3), full_matrices=False)
+    U3,_,_ = np.linalg.svd(ten3_unfold(tenX,3), full_matrices=False)
     U3 = U3[:,0:R3]
     # Core ...
     ten_core = ten3_nmode_product(tenX,np.conjugate(U1).T,1)
@@ -220,12 +220,17 @@ def mls_kraof(X,ra,rb,rc):
     C_hat = np.zeros((rc,cc)) + 1j*np.zeros((rc,cc))
     for i in range(cc):
         ci = X[:,i].reshape(ra,rb,rc)
-        ci = np.moveaxis(ci,1,2)
-        Uc,Sc,_ = np.linalg.svd(ten3_unfold(ci,1),full_matrices=False)
-        Ub,Sb,_ = np.linalg.svd(ten3_unfold(ci,2),full_matrices=False)
-        Ua,Sa,_ = np.linalg.svd(ten3_unfold(ci,3),full_matrices=False)
-        C_hat[:,i] = (Sc[0])**(1/3) * Uc[:,0]
-        B_hat[:,i] = (Sb[0])**(1/3) * Ub[:,0]
-        A_hat[:,i] = (Sa[0])**(1/3) * Ua[:,0]
-    return C_hat, B_hat, A_hat
+        ci = np.moveaxis(ci,1,2)   # rank-1 tensor
+        # HOSVD procedure ...
+        Uc,_,_ = np.linalg.svd(ten3_unfold(ci,1),full_matrices=False)
+        Ub,_,_ = np.linalg.svd(ten3_unfold(ci,2),full_matrices=False)
+        Ua,_,_ = np.linalg.svd(ten3_unfold(ci,3),full_matrices=False)
+        tenG = ten3_nmode_product(ci,Uc.conjugate().T,1)
+        tenG = ten3_nmode_product(tenG,Ub.conjugate().T,2)
+        tenG = ten3_nmode_product(tenG,Ua.conjugate().T,3)
+        # Rank-1 approximation procedure ...
+        C_hat[:,i] = (tenG[0,0,0])**(1/3) * Uc[:,0]
+        B_hat[:,i] = (tenG[0,0,0])**(1/3) * Ub[:,0]
+        A_hat[:,i] = (tenG[0,0,0])**(1/3) * Ua[:,0]
+    return A_hat, B_hat, C_hat
     
--- 005-multilinear-algebra-homeworks/homework-9/homework-9.py
+++ 005-multilinear-algebra-homeworks/homework-9/homework-9.py
@@ -8,38 +8,39 @@ import matplotlib.pyplot as plt
 import scipy as sp
 import scipy.linalg
 
-'''
 # Problem 1 ...
-I1 = 2
-I2 = 3
-I3 = 4
+I1 = 3
+I2 = 4
+I3 = 5
 R = 3
 
 A1 = (1/np.sqrt(2)) * (np.random.normal(0,1,(I1,R)) + 1j*np.random.normal(0,1,(I1,R)))
 A2 = (1/np.sqrt(2)) * (np.random.normal(0,1,(I2,R)) + 1j*np.random.normal(0,1,(I2,R)))
 A3 = (1/np.sqrt(2)) * (np.random.normal(0,1,(I3,R)) + 1j*np.random.normal(0,1,(I3,R)))
 X = mf.khatri_rao_product(mf.khatri_rao_product(A1,A2),A3)
+A1_hat, A2_hat, A3_hat = mf.mls_kraof(X,I1,I2,I3)
+X_hat = mf.khatri_rao_product(mf.khatri_rao_product(A1_hat,A2_hat),A3_hat)
 
-A3_hat,A2_hat,A1_hat = mf.mls_kraof(X,I1,I2,I3)
+NMSE_X = mf.nmse(X,X_hat)
 
 scaA1 = A1_hat/A1
-A1_hat = np.dot(A1_hat,np.linalg.inv(np.diag(scaA1[1,:])))
+A1_hat = np.dot(A1_hat,np.linalg.inv(np.diag(scaA1[0,:])))
 NMSE_A1 = mf.nmse(A1,A1_hat)
 
 scaA2 = A2_hat/A2
-A2_hat = np.dot(A2_hat,np.linalg.inv(np.diag(scaA2[1,:])))
+A2_hat = np.dot(A2_hat,np.linalg.inv(np.diag(scaA2[0,:])))
 NMSE_A2 = mf.nmse(A2,A2_hat)
 
 scaA3 = A3_hat/A3
-A3_hat = np.dot(A3_hat,np.linalg.inv(np.diag(scaA3[1,:])))
+A3_hat = np.dot(A3_hat,np.linalg.inv(np.diag(scaA3[0,:])))
 NMSE_A3 = mf.nmse(A3,A3_hat)
 
 print("Problem 1 ...")
+print(f"NMSE_X = {NMSE_X}")
 print(f"NMSE_A1 = {NMSE_A1}")
 print(f"NMSE_A2 = {NMSE_A2}")
 print(f"NMSE_A3 = {NMSE_A3}")
 print("")
-'''
 
 # Problem 2 ...
 I = 2
@@ -51,27 +52,16 @@ SNR = range(0,35,5)
 monte_carlo = int(1e+3)
 
 NMSE = []
-
 for snr in SNR:
     nmse = []
     for j in range(monte_carlo):
-        A = np.random.randn(I,R) + 1j * np.random.randn(I,R)
-        B = np.random.randn(J,R) + 1j * np.random.randn(J,R)
-        C = np.random.randn(K,R) + 1j * np.random.randn(K,R)
+        A = (1/np.sqrt(2)) * (np.random.normal(0,1,(I,R)) + 1j*np.random.normal(0,1,(I,R)))
+        B = (1/np.sqrt(2)) * (np.random.normal(0,1,(J,R)) + 1j*np.random.normal(0,1,(J,R)))
+        C = (1/np.sqrt(2)) * (np.random.normal(0,1,(K,R)) + 1j*np.random.normal(0,1,(K,R)))
         Xo = mf.khatri_rao_product(mf.khatri_rao_product(A,B),C)
         X = mf.awgn_noise(Xo,snr) 
 
-        C_hat,B_hat,A_hat = mf.mls_kraof(X,I,J,K)
-
-        scaA = A_hat/A
-        A_hat = np.dot(A_hat,np.linalg.inv(np.diag(scaA[1,:])))
-
-        scaB = B_hat/B
-        B_hat = np.dot(B_hat,np.linalg.inv(np.diag(scaB[1,:])))
-
-        scaC = C_hat/C
-        C_hat = np.dot(C_hat,np.linalg.inv(np.diag(scaC[1,:])))
-
+        A_hat,B_hat,C_hat = mf.mls_kraof(X,I,J,K)
         X_hat = mf.khatri_rao_product(mf.khatri_rao_product(A_hat,B_hat),C_hat)
         nmse.append(mf.nmse(Xo,X_hat))
         
@@ -86,6 +76,3 @@ ax.set_xlabel("SNR(dB)")
 ax.set_ylabel("NMSE(X)")
 plt.grid()
 plt.show()
-
-
-
